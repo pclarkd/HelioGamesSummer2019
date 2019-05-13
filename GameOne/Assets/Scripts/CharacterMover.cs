b69d8ecadeb8b413dc2344efff07308374c4d8ba
SpencerChugg <sdchugg@gmail.com>
2019-05-13 14:33:44 -0600

    Updates Ignore File

---
+++ GameOne/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int TestCode = 0;
+        if (TestCode = 0)
+        {
+            Console.WriteLine("The variable is set to true.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
