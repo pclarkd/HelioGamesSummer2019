8d7a01f59fc45e99e253746dd61a2d7eadd7bedf
Anthony Romrell <anthonyromrell@mac.com>
2019-05-13 14:52:51 -0600

    Upgraded to 2019

---
+++ GameOne/Assets/Scripts/CharacterJump.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Anthony Said Hi!!
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
